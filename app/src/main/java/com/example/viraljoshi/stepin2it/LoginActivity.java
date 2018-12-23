e1466ac8c8ddf05f8d24134e06aa57e6a4b6d7df
joshiviral <viraljoshi357814@gmail.com>
2018-12-22 23:03:21 -0500

    Sharedpreferences with Login

--- app/src/main/java/com/example/viraljoshi/stepin2it/LoginActivity.java
+++ app/src/main/java/com/example/viraljoshi/stepin2it/LoginActivity.java
@@ -1,4 +1,24 @@
 package com.example.viraljoshi.stepin2it;
 
-public class LoginActivity {
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private SharedPreferenceConfig preferenceConfig;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        preferenceConfig= new SharedPreferenceConfig(getApplicationContext());
+    }
+
+    public void userlogout(View view) {
+        preferenceConfig.writeLoginStatus(false);
+        startActivity(new Intent(this,MainActivity.class));
+        finish();
+    }
+
 }
--- app/src/main/java/com/example/viraljoshi/stepin2it/MainActivity.java
+++ app/src/main/java/com/example/viraljoshi/stepin2it/MainActivity.java
@@ -1,13 +1,51 @@
 package com.example.viraljoshi.stepin2it;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SharedPreferenceConfig sharedpreferenceConfig;
+    private EditText username,password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sharedpreferenceConfig = new SharedPreferenceConfig(getApplicationContext());
+
+        username=findViewById(R.id.etx_username);
+        password=findViewById(R.id.etx_password);
+
+        if(sharedpreferenceConfig.readLoginStatus())
+        {
+            startActivity(new Intent(this,LoginActivity.class));
+            finish();
+        }
+    }
+    public void loginUser(View view)
+    {
+        String user_name=username.getText().toString();
+        String user_password=password.getText().toString();
+
+
+
+        if(user_name.equals(getResources().getString(R.string.user_name))&& user_password.equals(getResources().getString(R.string.password)))
+        {
+            startActivity(new Intent(this,LoginActivity.class));
+             sharedpreferenceConfig.writeLoginStatus(true);
+             finish();
+        }
+        else
+            {
+                Toast.makeText(this,"LoginFailed..",Toast.LENGTH_SHORT).show();
+                username.setText("");
+                password.setText("");
+            }
     }
 }
